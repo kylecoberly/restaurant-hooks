e6c595f03c12edc3c1700cd693fdd633025fb00b
Kyle Coberly <kyle.coberly@gmail.com>
2020-06-11 14:07:13 -0600

    Playing hooky

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    isSwitchedOn: false,
-    menu: [],
+function App (){
+  const [isSwitchedOn, setIsSwitchedOn] = useState(false)
+  const [menu, setMenu] = useState([])
+
+  const handleLightToggle = () => {
+    setIsSwitchedOn(!isSwitchedOn)
   }
 
-  handleLightToggle = () => {
-    this.setState({ isSwitchedOn: !this.state.isSwitchedOn });
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon")
       .then(response => response.json())
       .then(response => {
         const names = response.results.map(pokemon => pokemon.name)
-        this.setState({ menu: names })
+        setMenu(names)
       })
-  }
+  })
 
-  render(){
-    const pokemon = this.state.menu.map(pokemon => {
-      return <li key={pokemon}>{pokemon}</li>
-    })
-    return (
-      <div className="App">
-        <p>The light is { this.state.isSwitchedOn ? "on" : "off" }</p>
-        <button onClick={this.handleLightToggle}>Toggle Light</button>
-        <ul> {pokemon} </ul>
-      </div>
-    );
-  }
+  const pokemon = menu.map(pokemon => {
+    return <li key={pokemon}>{pokemon}</li>
+  })
+
+  return (
+    <div className="App">
+      <p>The light is { isSwitchedOn ? "on" : "off" }</p>
+      <button onClick={handleLightToggle}>Toggle Light</button>
+      <ul> {pokemon} </ul>
+    </div>
+  );
 }
 
 export default App;
